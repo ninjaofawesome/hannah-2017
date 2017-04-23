de437a6dcffeada9651380c21eb06d077edb1ca4
Hannah Nordgren <hey@hereshannahs.info>
2017-04-22 21:52:03 -0400

    [FEATURE] added list of countries to sidebar

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import SidebarData from './sidebar.json';
 
 class Sidebar extends Component {
 
+  listCountries(SidebarData) {
+
+    if (!SidebarData) {
+      return null;
+    }
+
+    const visitedCountries = SidebarData.countries.map((place, index) => {
+      return <li className="countries-list__item">{place.country}</li>;
+    });
+
+    return visitedCountries;
+  }
+
   render() {
+
     return(
       <div className="countries-list--title">
         <h2> This is a sidebar list.</h2>
         <ul className="countries-list">
-          <li className="countries-list__item">Item</li>
+          {this.listCountries(SidebarData)}
         </ul>
       </div>
     );
